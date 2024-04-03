a67d600a3e0a3dda656c25e73bca691f7e8a7f3b
Vicky Le <vle04@student.rtc.edu>
2024-04-02 19:05:16 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // To get your proect setup with github
+            // 1. Open View -> GitChanges
+
+            // "Hello, World"
+            // Doube quotes tell the computer that everything in between is a STRING
+            Console.WriteLine("Hello, Mochi!");
+            // Welcome to Programming 120
+            Console.Write("Welcome to ");
+            Console.WriteLine("Programming 120");
+
+            //Console.Write("Please enter your name: ");
+            //string username = Console.ReadLine();
+            //Console/WriteLIne("Welcome " + username);
+
+
+
+            // To comment Code, do double forward//
+            //Console gives us access to working with the condole//
+            // CODE IS CASE SENSITIVE
+            // Dot, ., is used to access the different ways to interact with your code
+            // Parentheses are used to RUN our code, and pass in different arguments
+            // semi colon, the end of a single statement
+            // Curly braces defind code blocks
+            // Compile Time Error
+            //Is an error that visual studio will warn you about, that there is improper syntax someplace. Needs to be fixed before you can build your code. 
+            // Errors in the Error list will "Help" you find the problem
+        } // End of Main
+    } // End of class
+} // End of namespace
